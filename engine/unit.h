8e09ad210ba4403dae585081d9d7be4c4e076247
Lebogang Masia <lebogangmasia0@gmail.com>
2026-03-16 17:56:11 +0200

    Modified UnitTensor<U> to hold a matrix defined in Lingebra

--- engine/unit.h
+++ engine/unit.h
@@ -1,43 +1,41 @@
-#include <concepts>
-#include <type_traits>
+#include "include/matrix.hpp"
 #include <memory>
+#include <variant>
 #include <vector>
 #include <string>
 #include <functional>
-#include <cmath>
-#include <ostream>
 #include <algorithm>
 #include <unordered_set>
 
-#ifndef UNIT_GRAD_H
-#define UNIT_GRAD_H
+#ifndef UNIT_GRAD_HPP
+#define UNIT_GRAD_HPP
 
 namespace UnitGrad {
-    template <typename T>
-    concept Numeric = std::is_arithmetic_v<T>;
-
-    template <Numeric U>
+    template <typename U>
     class UnitTensor {
         public:
             using Ptr = std::shared_ptr<UnitTensor<U>>;
-    
+
             U data;
-            U grad = 0;
+            U grad;
             std::vector<Ptr> prev {};
             std::string op;
             std::function<void()> _backward = [](){};
-    
-            UnitTensor(U data_, std::vector<Ptr> children={}, std::string op_="") : data(data_), prev(children), op(op_) {};
-    
+
+            UnitTensor(U data_, std::vector<Ptr> children={}, std::string op_="") : data(data_), prev(children), op(op_) {
+                auto shape = data.shape();
+                grad = U(shape[0], shape[1]);
+            };
+
             static Ptr make(U data_) {
                 return std::make_shared<UnitTensor<U>>(data_);
             }
-    
+
             friend Ptr operator+(const Ptr& l, const Ptr& r) {
                 Ptr out = UnitTensor<U>::make(l->data + r->data);
                 out->prev = {l, r};
                 out->op = "+";
-    
+
                 UnitTensor<U>* out_ptr = out.get();
                 out->_backward = [l, r, out_ptr] {
                     l->grad += out_ptr->grad;
@@ -47,46 +45,42 @@ namespace UnitGrad {
             }
 
             friend Ptr operator-(const Ptr& l, const Ptr& r) {
-                Ptr out = UnitTensor<U>::make(l->data - r->data);
+                Ptr out = UnitTensor<U>::make(l->data - r->rdata);
                 out->prev = {l, r};
                 out->op = "-";
-    
+                
                 UnitTensor<U>* out_ptr = out.get();
                 out->_backward = [l, r, out_ptr] {
                     l->grad += out_ptr->grad;
                     r->grad -= out_ptr->grad;
                 };
-                return out;
             }
- 
-    
+
             friend Ptr operator*(const Ptr& l, const Ptr& r) {
                 Ptr out = UnitTensor<U>::make(l->data * r->data);
                 out->prev = {l, r};
-                out->op = "*";
-    
+                out->op = "matmul";
+
                 UnitTensor<U>* out_ptr = out.get();
                 out->_backward = [l, r, out_ptr] {
-                    l->grad += r->data * out_ptr->grad;
-                    r->grad += l->data * out_ptr->grad;
+                    l->grad += out_ptr->grad.matmul(r->data.transpose());
+                    r->grad += l->data.transpose().matmul(out_ptr->grad);
                 };
                 return out;
             }
-    
+
             friend Ptr relu(const Ptr& u) {
-                Ptr out = UnitTensor<U>::make(std::max(static_cast<U>(0), u->data));
+                auto relu_func = [](float x) -> float { return x > 0 ? x : 0.0f; };
+                Ptr out = UnitTensor<U>::make(u->data.map(relu_func));
                 out->prev = {u};
                 out->op = "ReLU";
-    
+
                 UnitTensor<U>* out_ptr = out.get();
                 out->_backward = [u, out_ptr] {
-                    u->grad += u->data > 0 ? out_ptr->grad : 0;
+                    auto d_relu = [](float x) -> float { return x > 0 ? 1.0f : 0.0f; };
+                    U deriv_mat = u->data.map(d_relu);
+                    u->grad += out_ptr->grad.element_wise(deriv_mat);
                 };
-                return out;
-            }
-    
-            friend std::ostream& operator<<(std::ostream& out, const Ptr& u) {
-                out << "Unit(data=" << u->data << ", grad=" << u->grad << ')';
                 return out;
             }
 
@@ -102,13 +96,17 @@ namespace UnitGrad {
                         topo.push_back(p);
                     }
                 };
-
                 build_topo(root);
-                root->grad = static_cast<U>(1);
+
+                auto shape = root->data.shape();
+                std::size_t total_elements = shape[0] * shape[1];
+                for (std::size_t i = 0; i < total_elements; i++) {
+                    root->grad.data_ptr()[i] = 1.0f;
+                }
                 std::reverse(topo.begin(), topo.end());
                 for (const Ptr& child: topo) child->_backward();
             }
     };
 }
 
-#endif // UNIT_GRAD_H
+#endif // UNIT_GRAD_HPP
